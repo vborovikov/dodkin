b18177629362a58e8c09ca145567ba4bea31e01b
Vladislav Borovikov <vborovikov@live.ru>
2023-05-25 23:46:12 +0500

    refactored the parser and added xml comments

--- src/Dodkin.Dispatch/AssemblyQualifiedTypeName.cs
+++ src/Dodkin.Dispatch/AssemblyQualifiedTypeName.cs
@@ -2,12 +2,27 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+/// <summary>
+/// Parses and represents an assembly qualified type name.
+/// </summary>
 sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, ISpanParsable<AssemblyQualifiedTypeName>
 {
 #pragma warning disable CS8618
+    /// <summary>
+    /// Creates a new instance of the <see cref="AssemblyQualifiedTypeName"/> class.
+    /// </summary>
     private AssemblyQualifiedTypeName() { }
 #pragma warning restore CS8618
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="AssemblyQualifiedTypeName"/> class with the specified values.
+    /// </summary>
+    /// <param name="fullName">The full name of the type.</param>
+    /// <param name="assemblyName">The name of the assembly that the type is defined in.</param>
+    /// <param name="version">The version of the assembly that the type is defined in.</param>
+    /// <param name="culture">The culture of the assembly that the type is defined in.</param>
+    /// <param name="publicKeyToken">The public key token of the assembly that the type is defined in.</param>
+    /// <param name="genericParameters">The generic type parameters of the type, if it is a generic type.</param>
     private AssemblyQualifiedTypeName(string fullName, string assemblyName, string version, string culture, string publicKeyToken, AssemblyQualifiedTypeName[] genericParameters)
     {
         this.FullName = fullName;
@@ -18,13 +33,37 @@ sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, I
         this.GenericParameters = genericParameters;
     }
 
+    /// <summary>
+    /// Gets the full name of the type.
+    /// </summary>
     public string FullName { get; }
+    /// <summary>
+    /// Gets the name of the assembly that the type is defined in.
+    /// </summary>
     public string AssemblyName { get; }
+    /// <summary>
+    /// Gets the version of the assembly that the type is defined in.
+    /// </summary>
     public string Version { get; }
+    /// <summary>
+    /// Gets the culture of the assembly that the type is defined in.
+    /// </summary>
     public string Culture { get; }
+    /// <summary>
+    /// Gets the public key token of the assembly that the type is defined in.
+    /// </summary>
     public string PublicKeyToken { get; }
+    /// <summary>
+    /// Gets the generic type parameters of the type, if it is a generic type.
+    /// </summary>
     public AssemblyQualifiedTypeName[] GenericParameters { get; }
 
+    /// <summary>
+    /// Tries to parse the specified assembly qualified type name.
+    /// </summary>
+    /// <param name="typeName">The assembly qualified type name to parse.</param>
+    /// <param name="assemblyQualifiedTypeName">The parsed assembly qualified type name, if parsing succeeded.</param>
+    /// <returns>True if the assembly qualified type name was successfully parsed, false otherwise.</returns>
     public static bool TryParse(ReadOnlySpan<char> typeName, [MaybeNullWhen(false)] out AssemblyQualifiedTypeName assemblyQualifiedTypeName)
     {
         var typeNameEnumerator = EnumerateTypeNames(typeName);
@@ -90,6 +129,11 @@ sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, I
         return true;
     }
 
+    /// <summary>
+    /// Parses the specified assembly qualified type name.
+    /// </summary>
+    /// <param name="typeName">The assembly qualified type name to parse.</param>
+    /// <returns>The parsed assembly qualified type name.</returns>
     public static AssemblyQualifiedTypeName Parse(ReadOnlySpan<char> typeName)
     {
         if (!TryParse(typeName, out var assemblyQualifiedTypeName))
@@ -102,15 +146,19 @@ sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, I
 
     private static TypeNameEnumerator EnumerateTypeNames(ReadOnlySpan<char> typeNames) => new(typeNames);
 
+    /// <inheritdoc />
     public static AssemblyQualifiedTypeName Parse(string s, IFormatProvider? provider) =>
         Parse(s.AsSpan(), provider);
 
+    /// <inheritdoc />
     public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out AssemblyQualifiedTypeName result) =>
         TryParse(s.AsSpan(), provider, out result);
 
+    /// <inheritdoc />
     public static AssemblyQualifiedTypeName Parse(ReadOnlySpan<char> s, IFormatProvider? provider) =>
         Parse(s);
 
+    /// <inheritdoc />
     public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, [MaybeNullWhen(false)] out AssemblyQualifiedTypeName result) =>
         TryParse(s, out result);
 
@@ -141,12 +189,12 @@ sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, I
     /// </summary>
     private ref struct TypeNamePartEnumerator
     {
-        private ReadOnlySpan<char> span;
+        private PartEnumerator enumerator;
         private TypeNamePartKind kind;
 
         public TypeNamePartEnumerator(ReadOnlySpan<char> span)
         {
-            this.span = span;
+            this.enumerator = new(span);
             this.Current = default;
             this.kind = TypeNamePartKind.Unknown;
         }
@@ -157,35 +205,12 @@ sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, I
 
         public bool MoveNext()
         {
-            if (this.span.IsEmpty)
-                return false;
-
-            var brackets = 0;
-            for (var i = 0; i != this.span.Length; ++i)
+            if (this.enumerator.MoveNext())
             {
-                if (this.span[i] == '[')
-                {
-                    ++brackets;
-                }
-                else if (this.span[i] == ']')
-                {
-                    --brackets;
-                }
-                else if (this.span[i] == ',' && brackets == 0)
-                {
-                    this.Current = new(this.span[..i], ++this.kind);
-
-                    this.span = this.span[i..];
-                    var n = this.span.IndexOfAnyExcept(',', ' ');
-                    this.span = n > 0 ? this.span[n..] : ReadOnlySpan<char>.Empty;
-
-                    return true;
-                }
+                this.Current = new(this.enumerator.Current, ++this.kind);
+                return true;
             }
-
-            this.Current = new(this.span, ++this.kind);
-            this.span = ReadOnlySpan<char>.Empty;
-            return true;
+            return false;
         }
     }
 
@@ -194,19 +219,38 @@ sealed class AssemblyQualifiedTypeName : IParsable<AssemblyQualifiedTypeName>, I
     /// </summary>
     private ref struct TypeNameEnumerator
     {
-        private ReadOnlySpan<char> span;
+        private PartEnumerator enumerator;
 
         public TypeNameEnumerator(ReadOnlySpan<char> span)
         {
-            this.span =
+            this.enumerator = new(
                 span.IsEmpty || span.IsWhiteSpace() ? ReadOnlySpan<char>.Empty :
                 span[0] == '[' && span[^1] == ']' ? span[1..^1] :
-                span;
+                span);
+        }
+
+        public ReadOnlySpan<char> Current => this.enumerator.Current;
+
+        public TypeNameEnumerator GetEnumerator() => this;
+
+        public bool MoveNext() => this.enumerator.MoveNext();
+    }
+
+    /// <summary>
+    /// Enumerates comma separated parts of a type name.
+    /// </summary>
+    private ref struct PartEnumerator
+    {
+        private ReadOnlySpan<char> span;
+
+        public PartEnumerator(ReadOnlySpan<char> span)
+        {
+            this.span = span;
         }
 
         public ReadOnlySpan<char> Current { get; private set; }
 
-        public TypeNameEnumerator GetEnumerator() => this;
+        public PartEnumerator GetEnumerator() => this;
 
         public bool MoveNext()
         {
