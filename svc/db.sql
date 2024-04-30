b43b5a00c69ed6b3ced82fea530e2849072660f7
Vladislav Borovikov <vborovikov@live.ru>
2024-04-30 21:40:13 +0500

    updated db collation

--- svc/db.sql
+++ svc/db.sql
@@ -10,7 +10,7 @@ end;
 if @@Error = 3702
    RaisError('Cannot delete the database because of the open connections.', 127, 127) with nowait, log;
 
-create database Dodkin collate SQL_Latin1_General_CP1_CI_AS;
+create database Dodkin collate Latin1_General_100_CI_AS_SC;
 go
 
 use Dodkin;
